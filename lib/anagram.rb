4999c0743e79879704afbb4bea91892edf47d1e7
Paul <paul.kiplagt@student.moringaschool.com>
2022-08-17 18:35:12 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,3 +6,8 @@ class Anagram
     def initialize(word)
       @name = word 
     end   
+    def match(array)
+        array.select {|x| x.split("").sort == @name.split("").sort}
+      end   
+    
+    end 
